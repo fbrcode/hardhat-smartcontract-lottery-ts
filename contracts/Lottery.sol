53f1039f79fc868ec036128cae89eb18a5ea2897
fbrcode <fabio.bressler@gmail.com>
2022-07-08 16:57:15 -0300

    basic contract compilation test

---
+++ contracts/Lottery.sol
@@ -0,0 +1,13 @@
+// Lottey functionality
+// Enter the lottery (paying amount of ether)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completely automated
+// Offchain Oracle -> randomness (Chainlink VRF) & automated execution (Chainlink Keeper)
+// Payout to the winner
+
+// SPDX-License-Identifier: MIT
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+contract Lottery {}
