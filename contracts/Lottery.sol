6069161e16fde8b975924a17d9750352ba069383
fbrcode <fabio.bressler@gmail.com>
2022-07-10 07:48:44 -0300

    add players management

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -26,6 +26,7 @@ contract Lottery {
   // 6.a: Type declarations
   // 6.b: State variables
   uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
 
   // 6.c: Events
   // 6.d: Modifiers
@@ -44,12 +45,26 @@ contract Lottery {
     if (msg.value < i_entranceFee) {
       revert Lottery__NotEnoughEthEntered();
     }
+    s_players.push(payable(msg.sender));
+    // tip: emit event when updating a dynamic array or mapping
+    // reference: https://docs.soliditylang.org/en/v0.8.13/introduction-to-smart-contracts.html#logs
+    /** It is possible to store data in a specially indexed data structure that maps all the way up to the block level. 
+        This feature called logs is used by Solidity in order to implement events. 
+        Contracts cannot access log data after it has been created, 
+        but they can be efficiently accessed from outside the blockchain. 
+        Since some part of the log data is stored in bloom filters, 
+        it is possible to search for this data in an efficient and cryptographically secure way, 
+        so network peers that do not download the whole blockchain (so-called “light clients”) can still find these logs. */
   }
 
   // 6.e.6: Internal
   // 6.e.7: Private
   // 6.e.8: View / Pure
-  function getEntranceFe() public view returns (uint256) {
+  function getEntranceFee() public view returns (uint256) {
     return i_entranceFee;
+  }
+
+  function getPlayers(uint256 index) public view returns (address) {
+    return s_players[index];
   }
 }
