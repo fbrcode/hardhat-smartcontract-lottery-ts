2518e59ec8c7aa2227bef22c8d44c20e2f48095b
fbrcode <fabio.bressler@gmail.com>
2022-07-13 09:06:25 -0300

    helper config adjusted

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,6 +1,17 @@
 const { ethers } = require("hardhat");
 
 const networkConfig = {
+  default: {
+    name: "hardhat",
+    keepersUpdateIntervalSeconds: "30",
+  },
+  31337: {
+    name: "hardhat",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    callbackGasLimit: "500000", // 500,000 gas
+    keepersUpdateIntervalSeconds: "30",
+  },
   4: {
     name: "rinkeby",
     vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
@@ -8,7 +19,7 @@ const networkConfig = {
     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     subscriptionId: "5959",
     callbackGasLimit: "500000", // 500,000 gas
-    secondsInterval: "30",
+    keepersUpdateIntervalSeconds: "30",
   },
   137: {
     name: "polygon",
@@ -17,14 +28,7 @@ const networkConfig = {
     gasLane: "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
     subscriptionId: "5959", // need to be created
     callbackGasLimit: "500000", // 500,000 gas
-    secondsInterval: "30",
-  },
-  31337: {
-    name: "hardhat",
-    entranceFee: ethers.utils.parseEther("0.01"),
-    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    callbackGasLimit: "500000", // 500,000 gas
-    secondsInterval: "30",
+    keepersUpdateIntervalSeconds: "30",
   },
 };
 
