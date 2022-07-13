a2a07feb168d2c584596b4440679e4a521ffcdff
fbrcode <fabio.bressler@gmail.com>
2022-07-13 10:34:50 -0300

    fix/enhance lottery deployment

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -13,25 +13,39 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const chainId = network.config.chainId;
   let vrfCoordinatorV2Address, subscriptionId;
 
+  log(`Deploying lottery on network: ${network.name}`);
   if (developmentChains.includes(network.name)) {
-    const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
-    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
-    // extract subscription id from the mocked contract
-    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
-    const transactionReceipt = await transactionResponse.wait(1);
-    subscriptionId = transactionReceipt.events[0].args.subId;
-    // in a real contract / network, we need to fund the subscription
-    // you would need the link token on a real network
-    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, MOCK_VRF_SUB_FUND_AMOUNT);
+    try {
+      const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+      vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+      // extract subscription id from the mocked contract
+      const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+      const transactionReceipt = await transactionResponse.wait(1);
+      subscriptionId = transactionReceipt.events[0].args.subId;
+      // in a real contract / network, we need to fund the subscription
+      // you would need the link token on a real network
+      await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, MOCK_VRF_SUB_FUND_AMOUNT);
+    } catch (error) {
+      log(error);
+    }
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinator;
     subscriptionId = networkConfig[chainId].subscriptionId;
   }
 
+  // check if the vrf coordinator is deployed already before deploying the lottery
+  if (!vrfCoordinatorV2Address) {
+    log(
+      `No VRF coordinator address found for chain: ${chainId}. \nFor local chains the mock needs to be executed before deploying the lottery contract.`
+    );
+    return;
+  }
+  log(`VRF coordinator address: ${vrfCoordinatorV2Address}`);
+
   const entranceFee = networkConfig[chainId].entranceFee;
   const gasLane = networkConfig[chainId].gasLane;
   const callbackGasLimit = networkConfig[chainId].callbackGasLimit;
-  const secondsInterval = networkConfig[chainId].secondsInterval;
+  const keepersUpdateIntervalSeconds = networkConfig[chainId].keepersUpdateIntervalSeconds;
 
   const args = [
     vrfCoordinatorV2Address,
@@ -39,12 +53,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     gasLane,
     subscriptionId,
     callbackGasLimit,
-    secondsInterval,
+    keepersUpdateIntervalSeconds,
   ];
   const lottery = await deploy("Lottery", {
     from: deployer,
     args: args,
-    logs: true,
+    log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   });
   log(`Lottery deployed at ${lottery.address}`);
@@ -53,6 +67,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     log(`Verifying contract "${lottery.address}" with args [${args}]...`);
     await verify(lottery.address, args);
   }
+  log(`Lottery Deployed!`);
   log(`----------------------------------------------------------`);
 };
 
