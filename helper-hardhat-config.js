e014b5461b68885e228fdc2b7626bc7a6bd7b56e
fbrcode <fabio.bressler@gmail.com>
2022-07-13 20:43:28 -0300

    move contant to deployer scripts

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,6 +4,7 @@ const networkConfig = {
   default: {
     name: "hardhat",
     keepersUpdateIntervalSeconds: "30",
+    blockConfirmations: 1,
   },
   31337: {
     name: "hardhat",
@@ -11,6 +12,7 @@ const networkConfig = {
     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     callbackGasLimit: "500000", // 500,000 gas
     keepersUpdateIntervalSeconds: "30",
+    blockConfirmations: 1,
   },
   4: {
     name: "rinkeby",
@@ -20,6 +22,7 @@ const networkConfig = {
     subscriptionId: "5959",
     callbackGasLimit: "500000", // 500,000 gas
     keepersUpdateIntervalSeconds: "30",
+    blockConfirmations: 6,
   },
   137: {
     name: "polygon",
@@ -29,20 +32,13 @@ const networkConfig = {
     subscriptionId: "5959", // need to be created
     callbackGasLimit: "500000", // 500,000 gas
     keepersUpdateIntervalSeconds: "30",
+    blockConfirmations: 6,
   },
 };
 
 const developmentChains = ["hardhat", "localhost"];
 
-// fixed constants for MockVrfCoordinator
-const MOCK_BASE_FEE = ethers.utils.parseEther("0.25"); // 0.25 LINK per request (premium)
-const MOCK_GAS_PRICE_LINK = 1e9; // 1 GWEI per LINK
-const MOCK_VRF_SUB_FUND_AMOUNT = "2"; // 2 LINK
-
 module.exports = {
   networkConfig,
   developmentChains,
-  MOCK_BASE_FEE,
-  MOCK_GAS_PRICE_LINK,
-  MOCK_VRF_SUB_FUND_AMOUNT,
 };
