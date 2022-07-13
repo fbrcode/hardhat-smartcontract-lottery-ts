8c714601640f467c81bc84818b548e755acd69bb
fbrcode <fabio.bressler@gmail.com>
2022-07-13 10:34:29 -0300

    enhance mock deployemnt

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,20 +1,24 @@
-const { developmentChains, MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK } = require("../helper-hardhat-config")
+const {
+  developmentChains,
+  MOCK_BASE_FEE,
+  MOCK_GAS_PRICE_LINK,
+} = require("../helper-hardhat-config");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    if (developmentChains.includes(network.name)) {
-        log(`Deploying mocks on local network: ${network.name}`)
-        // deploy a mock vrf coordinator
-        await deploy("VRFCoordinatorV2Mock", {
-            from: deployer,
-            args: [MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK],
-            logs: true,
-        })
-        log(`Mocks Deployed!`)
-        log(`----------------------------------------------------------`)
-    }
-}
+  if (developmentChains.includes(network.name)) {
+    log(`Deploying mocks on network: ${network.name}`);
+    // deploy a mock vrf coordinator
+    await deploy("VRFCoordinatorV2Mock", {
+      from: deployer,
+      args: [MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK],
+      log: true,
+    });
+    log(`Mocks Deployed!`);
+    log(`----------------------------------------------------------`);
+  }
+};
 
-module.exports.tags = ["all", "mocks"]
+module.exports.tags = ["all", "mocks"];
