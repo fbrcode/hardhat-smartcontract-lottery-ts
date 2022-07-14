254603a97cdf89b25dbc70d05bcf7d629186f8aa
fbrcode <fabio.bressler@gmail.com>
2022-07-13 21:16:03 -0300

    deploy mock with local constants

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,8 +1,7 @@
-const {
-  developmentChains,
-  MOCK_BASE_FEE,
-  MOCK_GAS_PRICE_LINK,
-} = require("../helper-hardhat-config");
+const { developmentChains } = require("../helper-hardhat-config");
+
+const MOCK_BASE_FEE = "250000000000000000"; // 0.25 is this the premium in LINK?
+const MOCK_GAS_PRICE_LINK = 1e9; // link per gas, is this the gas lane? // 0.000000001 LINK per gas
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
