dcc7220192693f1bb7b7e97000bb089c4ccce029
fbrcode <fabio.bressler@gmail.com>
2022-07-14 18:01:56 -0300

    typescript config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,30 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-deploy';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xKey";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+import 'dotenv/config';
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  defaultNetwork: "hardhat",
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import { HardhatUserConfig } from 'hardhat/config';
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xKey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key';
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 31337,
-      blockConfirmations: 1,
     },
     rinkeby: {
       url: RINKEBY_RPC_URL,
       chainId: 4,
-      blockConfirmations: 6,
       accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
@@ -35,11 +37,11 @@ module.exports = {
   },
   gasReporter: {
     enabled: true,
-    outputFile: "gas-reporter.log",
+    outputFile: 'gas-reporter.log',
     noColors: true,
-    currency: "USD",
+    currency: 'USD',
     coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "ETH", // how much it costs to deploy/run on ethereum
+    token: 'ETH', // how much it costs to deploy/run on ethereum
     // token: "MATIC", // how much it costs to deploy/run on polygon
   },
   namedAccounts: {
@@ -51,8 +53,10 @@ module.exports = {
       default: 1,
     },
   },
-  solidity: "0.8.7",
+  solidity: '0.8.7',
   mocha: {
     timeout: 900000, // 900 seconds max
   },
 };
+
+export default config;
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,44 +1,49 @@
-const { ethers } = require("hardhat");
+import { ethers } from 'hardhat';
 
-const networkConfig = {
-  default: {
-    name: "hardhat",
-    keepersUpdateIntervalSeconds: "30",
-    blockConfirmations: 1,
-  },
+type NetworkConfig = {
+  name: string;
+  entranceFee: string;
+  vrfCoordinator: string;
+  gasLane: string;
+  subscriptionId: string;
+  callbackGasLimit: string;
+  keepersUpdateIntervalSeconds: string;
+  blockConfirmations: number;
+};
+
+type NetworkConfigGroup = {
+  [key: number]: NetworkConfig;
+};
+
+export const networkConfig: NetworkConfigGroup = {
   31337: {
-    name: "hardhat",
-    entranceFee: ethers.utils.parseEther("0.01"),
-    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    callbackGasLimit: "500000", // 500,000 gas
-    keepersUpdateIntervalSeconds: "30",
+    name: 'hardhat',
+    entranceFee: ethers.utils.parseEther('0.01').toString(),
+    vrfCoordinator: '',
+    gasLane: '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
+    subscriptionId: '',
+    callbackGasLimit: '500000', // 500,000 gas
+    keepersUpdateIntervalSeconds: '30',
     blockConfirmations: 1,
   },
   4: {
-    name: "rinkeby",
-    vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-    entranceFee: ethers.utils.parseEther("0.01"),
-    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    subscriptionId: "5959",
-    callbackGasLimit: "500000", // 500,000 gas
-    keepersUpdateIntervalSeconds: "30",
+    name: 'rinkeby',
+    entranceFee: ethers.utils.parseEther('0.01').toString(),
+    vrfCoordinator: '0x6168499c0cFfCaCD319c818142124B7A15E857ab',
+    gasLane: '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
+    subscriptionId: '5959',
+    callbackGasLimit: '500000', // 500,000 gas
+    keepersUpdateIntervalSeconds: '30',
     blockConfirmations: 6,
   },
   137: {
-    name: "polygon",
-    ethUsdPriceFeed: "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed",
-    entranceFee: ethers.utils.parseEther("0.01"),
-    gasLane: "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
-    subscriptionId: "xxxx", // need to be created
-    callbackGasLimit: "500000", // 500,000 gas
-    keepersUpdateIntervalSeconds: "30",
+    name: 'polygon',
+    entranceFee: ethers.utils.parseEther('0.01').toString(),
+    vrfCoordinator: '',
+    gasLane: '0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f',
+    subscriptionId: 'xxxx', // need to be created
+    callbackGasLimit: '500000', // 500,000 gas
+    keepersUpdateIntervalSeconds: '30',
     blockConfirmations: 6,
   },
-};
-
-const developmentChains = ["hardhat", "localhost"];
-
-module.exports = {
-  networkConfig,
-  developmentChains,
 };
