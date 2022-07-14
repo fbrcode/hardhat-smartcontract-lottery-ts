96b87056528ed7ee3e0fd8df20a08c201a3612bb
fbrcode <fabio.bressler@gmail.com>
2022-07-14 11:54:46 -0300

    add save deployments to config and ignore on scm

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,10 +20,11 @@ module.exports = {
       blockConfirmations: 1,
     },
     rinkeby: {
+      url: RINKEBY_RPC_URL,
       chainId: 4,
       blockConfirmations: 6,
-      url: RINKEBY_RPC_URL,
       accounts: [PRIVATE_KEY],
+      saveDeployments: true,
     },
   },
   etherscan: {
@@ -43,7 +44,8 @@ module.exports = {
   },
   namedAccounts: {
     deployer: {
-      default: 0,
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
     player: {
       default: 1,
